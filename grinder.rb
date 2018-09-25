5e6f0e00fde72aa7699daa2e9892ea0714f8861d
William Francis <williamjfrancis2000@gmail.com>
2018-09-25 16:15:37 +1000

    Started building the selector

--- grinder.rb
+++ grinder.rb
@@ -2,23 +2,47 @@ require 'faker'
 
 
 class Grindr
+
+  def initialize
+    @men = []
+  end
+
   def grindr_feature
-    men = {}
+
+
     5.times do
-    men[Faker::Name.unique.male_first_name ] = { age: Faker::Number.between(21, 35), location: "Sydney", movie: Faker::Music.band, nationality: Faker::Nation.nationality }
+    @men << { name: Faker::Name.unique.male_first_name, age: Faker::Number.between(21, 35), location: "Sydney", movie: Faker::Music.band, nationality: Faker::Nation.nationality }
     end
 
+
     puts "You quickly navigate to Grinder your prefered 'dating' app, to find find that 'true love' of yours!"
-    sleep(2)
+    # sleep(2)
     puts "A list of matches is displayed. It appears that you have a premium subscription!"
-    sleep(1.5)
+    # sleep(1.5)
     puts "But of course you do! Who would you like to view?"
-    sleep(1)
-    puts men
-
-    response = gets.strip
+    # sleep(1)
 
 
+# This is just to see that it's working for now although I want to print it to screen like this
+    puts @men
+
+    puts "Type either 0, 1, 2, 3, 4 then hit enter!"
+
+    response = gets.strip.to_i
+
+      if response == 0
+        puts "You matched with #{}"
+      elsif response == 1
+        puts "w"
+      elsif response == 2
+        puts "e"
+      elsif response == 3
+        puts "r"
+      elsif response == 4
+        puts "t"
+      else
+        puts "Nothing"
+      end
   end
 end
 
