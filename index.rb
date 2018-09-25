3cbece5a2df3f3c111c459dbe836f56dd0cd4e0f
Luigio De Leon <ginodleon@gmail.com>
2018-09-25 16:28:33 +1000

    extend bar story

--- index.rb
+++ index.rb
@@ -1,3 +1,5 @@
+require_relative 'bar'
+
 puts "You just had a long week, it's Friday night and you've decided you want find your one 'true love'.  What you want to do playa?"
 puts "1. Social Media"
 puts "2. Going out"
@@ -7,7 +9,8 @@ while response = gets.strip
         puts "You grab your phone."
         break
     elsif response == "2"
-        puts "You grab your finest duds and head out"
+        puts "You grab your finest dudes and head out"
+        bar
         break
     elsif response != "1" || response != "2" 
         puts "Wrong answer bro!  Hit it again."
